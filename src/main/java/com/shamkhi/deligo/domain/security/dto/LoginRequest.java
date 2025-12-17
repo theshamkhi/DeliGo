14b55d7cb885763d6833a364dce86c83013de918
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-12-17 14:11:49 +0100

    feat(security): add LoginRequest DTO for authentication

---
+++ src/main/java/com/shamkhi/deligo/domain/security/dto/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.shamkhi.deligo.domain.security.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+    @NotBlank(message = "Le nom d'utilisateur est obligatoire")
+    private String username;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String password;
+}
