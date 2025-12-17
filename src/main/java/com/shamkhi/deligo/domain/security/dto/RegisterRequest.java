abade5057b360ee9b51f0897ca346c81953fa4a6
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-12-17 14:12:49 +0100

    feat(security): add RegisterRequest DTO for user registration

---
+++ src/main/java/com/shamkhi/deligo/domain/security/dto/RegisterRequest.java
@@ -0,0 +1,39 @@
+package com.shamkhi.deligo.domain.security.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    @NotBlank(message = "Le nom d'utilisateur est obligatoire")
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "L'email doit être valide")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
+    private String password;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    private String prenom;
+
+    private String telephone;
+    private Set<String> roles;
+    private String clientExpediteurId;
+    private String livreurId;
+}
