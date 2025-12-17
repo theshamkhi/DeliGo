439f83e2fda370bd2e8bd10fd8941c707f2d780b
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-12-17 14:12:20 +0100

    feat(security): add LoginResponse DTO with JWT token and user info

---
+++ src/main/java/com/shamkhi/deligo/domain/security/dto/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.shamkhi.deligo.domain.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponse {
+    private String token;
+    private String type = "Bearer";
+    private String id;
+    private String username;
+    private String email;
+    private String nom;
+    private String prenom;
+    private Set<String> roles;
+    private Set<String> permissions;
+}
