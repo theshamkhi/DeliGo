52fcb3795eba53cd7ad8bc8f45fc5e5b6fe97975
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-12-22 10:23:02 +0100

    feat(security): create DTOs for refresh token request and response

---
+++ src/main/java/com/shamkhi/deligo/domain/security/dto/RefreshTokenRequest.java
@@ -0,0 +1,14 @@
+package com.shamkhi.deligo.domain.security.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenRequest {
+    @NotBlank(message = "Le refresh token est obligatoire")
+    private String refreshToken;
+}
---
+++ src/main/java/com/shamkhi/deligo/domain/security/dto/TokenRefreshResponse.java
@@ -0,0 +1,16 @@
+package com.shamkhi.deligo.domain.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenRefreshResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String type = "Bearer";
+}
